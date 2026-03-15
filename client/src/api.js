32f52fcc97b137e3781ad188f0fc3a5038738d49
SunnyS214 <sonawanesunny69@gmail.com>
2026-03-15 14:25:24 +0530

    update frontend api url

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API= axios.create({
-    baseURL:"http://localhost:5000/api",
+    baseURL:"https://ai-journalllm.onrender.com/api",
     withCredentials:true
 })
 
